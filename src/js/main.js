c04483ac8e174fc4f5c7f53dce59ebe5dc3c4794
vanjazeli <vanjazeli@gmail.com>
2023-03-30 09:43:04 +0200

    feat: add banner section

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import '../styles/style.scss';
 import documentLoaded from './chunks/documentLoaded';
 import header from './chunks/header';
+import banner from './chunks/banner';
 
 documentLoaded.init();
 header.init();
+banner.init();
