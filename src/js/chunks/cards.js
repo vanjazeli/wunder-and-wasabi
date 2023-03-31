bf91b97508fab0cca162f1cd77f67cdff768e8ea
vanjazeli <vanjazeli@gmail.com>
2023-03-31 11:28:20 +0200

    feat: add cards section scroll animation

---
+++ src/js/chunks/cards.js
@@ -0,0 +1,57 @@
+import gsap from 'gsap';
+
+const cards = {
+	module: document.querySelector('.js-cards'),
+	rows: document.querySelectorAll('.js-cards-row'),
+
+	init: function () {
+		this.animateOnScroll();
+	},
+
+	strToBool: function (string) {
+		if (string === 'false') {
+			return false;
+		} else if (string === 'true') {
+			return true;
+		}
+	},
+
+	isInViewport: function (element) {
+		const rect = element.getBoundingClientRect();
+		const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+		const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+
+		const vertInView = rect.top <= windowHeight && rect.top + rect.height >= 0;
+		const horInView = rect.left <= windowWidth && rect.left + rect.width >= 0;
+
+		return vertInView && horInView;
+	},
+
+	animateOnScroll: function () {
+		window.addEventListener('scroll', () => {
+			const scrollPosition = window.scrollY;
+
+			this.rows.forEach((row) => {
+				if (this.isInViewport(this.module)) {
+					const animationDirection = this.strToBool(row.getAttribute('data-animation-direction'));
+
+					if (animationDirection) {
+						gsap.to(row, {
+							y: scrollPosition * 0.08,
+							duration: 0.2,
+							ease: 'easeInOut',
+						});
+					} else {
+						gsap.to(row, {
+							y: -scrollPosition * 0.08,
+							duration: 0.2,
+							ease: 'easeInOut',
+						});
+					}
+				}
+			});
+		});
+	},
+};
+
+export default cards;
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@ import '../styles/style.scss';
 import documentLoaded from './chunks/documentLoaded';
 import header from './chunks/header';
 import banner from './chunks/banner';
+import cards from './chunks/cards';
 
 documentLoaded.init();
 header.init();
 banner.init();
+cards.init();
