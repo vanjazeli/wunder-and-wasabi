d3744e24b07c81ab2513e27b29c47f7486a502f8
vanjazeli <vanjazeli@gmail.com>
2023-12-03 09:02:48 +0100

    feat: add image parallax in image-paragraph module

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -15,6 +15,22 @@ const helpers = {
 			this.scrollable = true;
 		}
 	},
+
+	isInView: function (element) {
+		const rect = element.getBoundingClientRect();
+		return rect.top <= (window.innerHeight || document.documentElement.clientHeight) && rect.bottom >= 0 && rect.left <= (window.innerWidth || document.documentElement.clientWidth) && rect.right >= 0;
+	},
+
+	getPosition: function (element) {
+		let xPosition = 0;
+		let yPosition = 0;
+		while (element) {
+			xPosition += element.offsetLeft - element.scrollLeft + element.clientLeft;
+			yPosition += element.offsetTop - element.scrollTop + element.clientTop;
+			element = element.offsetParent;
+		}
+		return { x: xPosition, y: yPosition };
+	},
 };
 
 export default helpers;
--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@ import documentLoaded from './chunks/documentLoaded';
 import header from './chunks/header';
 import banner from './chunks/banner';
 import cards from './chunks/cards';
+import imageParagraph from './chunks/imageParagraph';
 
 documentLoaded.init();
 header.init();
 banner.init();
 cards.init();
+imageParagraph.init();
